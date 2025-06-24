5a613a50cb41805ed9afe13c59c2352ef91e97d0
AkshithaED <154822131+AkshithaED@users.noreply.github.com>
2025-06-24 17:06:18 +0530

    Update views.py

--- product_app/views.py
+++ product_app/views.py
@@ -626,13 +626,7 @@ def release_product_image_list(request):
 
 #         except Patch.DoesNotExist:
 #             return Response({"error": "Patch not found"}, status=status.HTTP_404_NOT_FOUND)
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from .models import Patch, Product
-# Import your serializer
-from .serializers import PatchSerializer
+
 
 class PatchProductDetailView(APIView):
     def get(self, request, patch_name, product_name):
